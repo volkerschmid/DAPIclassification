a5a674a6f262b9e3a8d0e7bb20ab143398a3ee1e
Volker Schmid <volker.schmid@lmu.de>
2016-11-28 16:41:14 +0100

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,5 +1,4 @@
 setRepositories(ind=c(1,2))
-install.packages(c("EBImage","devtools"))
-library("devtools")
-install_github("bioimaginggroup/bioimagetools")
-install_bitbucket("bioimaginggroup/nucim")
+install.packages("nucim")
+# If you got an error message during installation of packages "tiff" and "fftw", you need to install some libraries.
+# For example in Ubuntu or Debian, use sudo apt install libtiff5-dev libfftw3-dev
